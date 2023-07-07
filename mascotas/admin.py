d97d7edfd8f24e5ebc79b97d43faa2f6664dd93a
nrocha02 <thewolf2732@gmail.com>
2023-07-07 17:43:41 -0400

    Boleta y det_boleta visible en el panel de admin

--- mascotas/admin.py
+++ mascotas/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from .models import Producto, Categoria
+from .models import *
 
 # Register your models here.
 admin.site.register(Producto)
 admin.site.register(Categoria)
+admin.site.register(Boleta)
+admin.site.register(detalle_boleta)
