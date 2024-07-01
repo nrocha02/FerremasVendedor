25b6e91ad6bbc6bb5be99d41f3e02bd2ccf308b7
nrocha02 <rocha.nico_19@protonmail.com>
2024-06-30 22:41:17 -0400

    Eliminación de comentarios innecesarios

--- payments/views.py
+++ payments/views.py
@@ -9,9 +9,9 @@ def start_payment(request):
         integration_type=settings.TRANSBANK['MODO']
     ))
 
-    amount = 10000  # Ejemplo de monto, puedes cambiar esto según tus necesidades
-    buy_order = "orden12345"  # Número de orden único
-    session_id = "sesion12345"  # ID de sesión
+    amount = 10000  
+    buy_order = "orden12345"  
+    session_id = "sesion12345"  
     return_url = request.build_absolute_uri('/payments/return/')
 
     response = tx.create(buy_order, session_id, amount, return_url)
@@ -30,10 +30,8 @@ def return_from_payment(request):
     response = tx.commit(token)
 
     if response['response_code'] == 0:
-        # El pago fue exitoso
         return redirect('/payments/finalize/?token_ws=' + token)
     else:
-        # El pago falló
         return render(request, 'payments/failure.html', {'response': response})
 
 def finalize_payment(request):
